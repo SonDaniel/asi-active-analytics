a408a1cff3b466bc9cd8bf5e4ec6e0b07d9bdddf
Daniel Son <sondaniel96@gmail.com>
2017-05-01 17:56:56 -0700

    added routing for app. added side menu for easy access added feedback page, homepage, log-data page, user-data page

---
+++ src/app/log-data/log-data.component.ts
@@ -0,0 +1,167 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { AjaxService } from '../../services/ajax.service';
+
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-log-data',
+  templateUrl: './log-data.component.html',
+  styleUrls: ['./log-data.component.css']
+})
+export class LogDataComponent implements OnInit {
+  @ViewChild('overallData') overallData : ElementRef;
+  overalldataChart : Chart;
+  
+  // variables for frontend
+  userCount: number;
+  feedbackCount : number;
+
+  userData : Array<Object>;
+  feedbackData : Array<Object>;
+  logData : Array<Object> = [];
+  rankData : Array<Object>;
+
+  monthData : Array<Object> = [];
+  totalLogs : number;
+
+  selected : any = -1;
+
+  constructor(private ajax: AjaxService) {}
+
+  ngOnInit() {
+    this.ajax.init().then(() => {
+      let getActivityLog = this.ajax.get('activity-logs/').then(res => {
+        this.logData = this.logData.concat(res.data);
+      });
+
+      let getEventLog = this.ajax.get('event-logs/').then(res => {
+        this.logData = this.logData.concat(res.data);
+      });
+
+      let getRank = this.ajax.get('users/rankings/').then(res => {
+        this.rankData = res.data;
+      });
+
+      Promise.all([getActivityLog, getEventLog]).then(() => {
+        this.totalLogs = this.logData.length;
+
+        this.logData.sort(function(left, right) : number {
+          return moment(left['datetime']).diff(moment(right['datetime']));
+        });
+
+        // let obj = this.getData('MMMM');
+        
+        let data = {
+          labels: this.logData.filter((obj) => {
+            return true;
+          }),
+          datasets: [{
+            label: 'Number of Logs',
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            data : []
+          }]
+        };
+
+        // this.monthData = this.combineData(data.labels, data.datasets[0].data);
+
+        let ctx = this.overallData.nativeElement.getContext('2d');
+
+        this.overalldataChart = new Chart(ctx, {
+          type: 'line',
+          data : data,
+          options: {
+
+          }
+        });
+      });
+    });
+  }
+
+  // getData(format: string, start: any = null, end: any = null) : Object {
+  //   let dates = [];
+  //   let data = [];
+
+  //   if(!start) {
+  //     start = moment(this.logData[0]['datetime']);
+  //   } else {
+  //     start = moment(start);
+  //   }
+
+  //   if(!end) {
+  //     end = moment(this.logData[this.logData.length - 1]['datetime']);
+  //   } else {
+  //     end = moment(end);
+  //   }
+
+  //   let dateComparable = start.format(format);
+  //   let dataObject = {
+  //     count : 0,
+  //     start: null,
+  //     end: null
+  //   }
+  //   for(let x in this.logData) {
+  //     let date = moment(this.logData[x]['datetime']);
+
+  //     if(start.diff(date) <= 0 && end.diff(date) >= 0) {
+  //       if(!dates.includes(date.format(format))) {
+  //         dates.push(date.format(format));
+  //       }
+
+  //       if(dateComparable === date.format(format)) {
+  //         dataObject.count++;
+  //       } else {
+  //         dateComparable = date.format(format);
+  //         dataObject.end = date;
+  //         data.push(dataObject);
+          
+  //         dataObject = {
+  //           count : 1,
+  //           start: date,
+  //           end: null
+  //         };
+  //       }
+  //     }
+  //   }
+
+  //   data.push(dataObject);
+
+  //   return {
+  //     'dates' : dates,
+  //     'data' : data
+  //   }
+  // }
+
+  combineData(labels : Array<String>, data : Array<Number>) : Array<Object> {
+    return data.map((x, i) => {
+      return {
+        'date': labels[i],
+        'data': x
+      }
+    });
+  }
+
+  selectList(event, selected : any) {
+    event.preventDefault();
+    this.selected = selected;
+    console.log(JSON.stringify(selected));
+
+    // let data = this.getData('M-DD', selected.data.start, selected.data.end);
+    // this.overalldataChart.datasets[0].data = 
+  }
+
+  onHover(event) {
+    //TO DO: Show different Graph and activate
+  }
+}
