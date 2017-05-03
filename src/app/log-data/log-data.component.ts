cbe4da9a54f42814291d7bff1524d4da56fa3545
Daniel Son <sondaniel96@gmail.com>
2017-05-03 14:30:59 -0700

    working on graph

--- src/app/log-data/log-data.component.ts
+++ src/app/log-data/log-data.component.ts
@@ -50,12 +50,15 @@ export class LogDataComponent implements OnInit {
           return moment(left['datetime']).diff(moment(right['datetime']));
         });
 
-        // let obj = this.getData('MMMM');
+        let dateSet = new Set();
         
+        // Extract datetime for LogData
+        for(let x of this.logData) {
+          dateSet.add(moment(x['datetime']).format('MMMM'));
+        }
+
         let data = {
-          labels: this.logData.filter((obj) => {
-            return true;
-          }),
+          labels: Array.from(dateSet),
           datasets: [{
             label: 'Number of Logs',
             backgroundColor: "rgba(75,192,192,0.4)",
@@ -70,11 +73,11 @@ export class LogDataComponent implements OnInit {
             pointHoverBackgroundColor: "rgba(75,192,192,1)",
             pointHoverBorderColor: "rgba(220,220,220,1)",
             pointHoverBorderWidth: 2,
-            data : []
+            data : this.countData('MMMM')
           }]
         };
 
-        // this.monthData = this.combineData(data.labels, data.datasets[0].data);
+        this.monthData = this.combineData(data.labels, data.datasets[0].data);
 
         let ctx = this.overallData.nativeElement.getContext('2d');
 
@@ -89,59 +92,41 @@ export class LogDataComponent implements OnInit {
     });
   }
 
-  // getData(format: string, start: any = null, end: any = null) : Object {
-  //   let dates = [];
-  //   let data = [];
+  countData(format: string, start: any = null, end: any = null) : Array<Number> {
+    let data = [];
 
-  //   if(!start) {
-  //     start = moment(this.logData[0]['datetime']);
-  //   } else {
-  //     start = moment(start);
-  //   }
+    if(!start) {
+      start = moment(this.logData[0]['datetime']);
+    } else {
+      start = moment(start);
+    }
 
-  //   if(!end) {
-  //     end = moment(this.logData[this.logData.length - 1]['datetime']);
-  //   } else {
-  //     end = moment(end);
-  //   }
+    if(!end) {
+      end = moment(this.logData[this.logData.length - 1]['datetime']);
+    } else {
+      end = moment(end);
+    }
 
-  //   let dateComparable = start.format(format);
-  //   let dataObject = {
-  //     count : 0,
-  //     start: null,
-  //     end: null
-  //   }
-  //   for(let x in this.logData) {
-  //     let date = moment(this.logData[x]['datetime']);
+    let dateComparable = start.format(format);
+    let count = 0;
+    for(let x in this.logData) {
+      let date = moment(this.logData[x]['datetime']);
 
-  //     if(start.diff(date) <= 0 && end.diff(date) >= 0) {
-  //       if(!dates.includes(date.format(format))) {
-  //         dates.push(date.format(format));
-  //       }
+      if(start.diff(date) <= 0 && end.diff(date) >= 0) {
+        if(dateComparable === date.format(format)) {
+          count++;
+        } else {
+          dateComparable = date.format(format);
+          data.push(count);
+          count = 1;
+        }
+      }
+    }
 
-  //       if(dateComparable === date.format(format)) {
-  //         dataObject.count++;
-  //       } else {
-  //         dateComparable = date.format(format);
-  //         dataObject.end = date;
-  //         data.push(dataObject);
-          
-  //         dataObject = {
-  //           count : 1,
-  //           start: date,
-  //           end: null
-  //         };
-  //       }
-  //     }
-  //   }
+    data.push(count);
 
-  //   data.push(dataObject);
-
-  //   return {
-  //     'dates' : dates,
-  //     'data' : data
-  //   }
-  // }
+    return data;
+  }
 
   combineData(labels : Array<String>, data : Array<Number>) : Array<Object> {
     return data.map((x, i) => {
@@ -156,9 +141,30 @@ export class LogDataComponent implements OnInit {
     event.preventDefault();
     this.selected = selected;
     console.log(JSON.stringify(selected));
+    var dateSet = new Set();
+    console.log(dateSet);
+    if(this.selected != -1) {
+      for(let x of this.logData) {
+        let date = moment(x['datetime']);
+        if(date.format('MMMM') === selected['date']) {
+          dateSet.add(date.format('MM-DD'));
+        }
+      }
 
-    // let data = this.getData('M-DD', selected.data.start, selected.data.end);
-    // this.overalldataChart.datasets[0].data = 
+      let dataArray = Array.from(dateSet);
+      this.overalldataChart.data.labels = dataArray;
+      this.overalldataChart.data.datasets[0].data = this.countData('MM-DD', `2017-${dataArray[0]}`, `2017-${dataArray[dataArray.length -1]}`);
+      console.log(this.overalldataChart.data.datasets[0].data);
+    } else {
+        for(let x of this.logData) {
+          dateSet.add(moment(x['datetime']).format('MMMM'));
+        }
+        this.overalldataChart.data.labels = Array.from(dateSet);
+        this.overalldataChart.data.datasets[0].data = this.countData('MMMM');
+        console.log(this.overalldataChart.data.datasets[0].data);
+    }
+
+    this.overalldataChart.update();
   }
 
   onHover(event) {
